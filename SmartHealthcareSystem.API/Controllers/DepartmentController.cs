e378c2704512aa3539004d0e4317d43dfd16a8bf
HusseinTahaa <tahahassein3@gmail.com>
2026-06-21 02:12:22 +0300

    Add API project

---
+++ SmartHealthcareSystem.API/Controllers/DepartmentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHealthcareSystem.Application.DTOs.Department;
+using SmartHealthcareSystem.Application.Services.Department;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartHealthcareSystem.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentService _Service;
+        public DepartmentController(IDepartmentService service)
+        {
+            _Service = service;
+        }
+
+        // GET: api/<DepartmentController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var departments =await _Service.GetAllDepartments();
+            return Ok(departments);
+
+        }
+
+        // GET api/<DepartmentController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var department =  await _Service.GetById(id);
+            return Ok(department);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var department = await _Service.GetByName(name);
+            return Ok(department);
+        }
+
+        // POST api/<DepartmentController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateDepartmentDto name)
+        {
+          var dep=  await _Service.Create(name);
+            return Ok(dep);
+        }
+
+        // PUT api/<DepartmentController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] UpdateDepartmentDto value)
+        {
+            var department = await _Service.GetById(id);
+            await _Service.Update(id, value);
+            return Ok(department);
+        }
+
+        // DELETE api/<DepartmentController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+           var department =  await _Service.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ SmartHealthcareSystem.API/Program.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using SmartHealthcareSystem.API.Middlewares;
+using SmartHealthcareSystem.Application.IRepository;
+using SmartHealthcareSystem.Application.Services.Auth;
+using SmartHealthcareSystem.Application.Services.Department;
+using SmartHealthcareSystem.Domain.Entities;
+using SmartHealthcareSystem.Infrastructure.Data;
+using SmartHealthcareSystem.Infrastructure.Repository;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityCore<AppUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(document=>new OpenApiSecurityRequirement
+    {
+        [
+            new OpenApiSecuritySchemeReference("bearer", document)
+        ] = []
+    });
+});
+
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDepartmentRepo, DepartmentRepo>();
+var app = builder.Build();
+app.UseMiddleware<GlobalExceptionMiddelware>();
+using (var  scope = app.Services.CreateScope())
+{
+    var RoleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = { "Receptionist", "Admin", "Doctor" };
+    foreach (var role in roles)
+    {
+        if (!await RoleManager.RoleExistsAsync(role)) {
+            await RoleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
+
+
+
+app.UseMiddleware<RequestLoggingMiddelware>();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+//app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
